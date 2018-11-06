92c162f5a1d579207d6cbdfb6e4657f373d9690d
Lanndon Rose <roselan@mail.gvsu.edu>
2018-11-05 23:44:04 -0500

    updated

--- client.py
+++ client.py
@@ -1,22 +1,22 @@
 
-# Python program to implement client side of chat room. 
-import socket 
-import select 
-import sys 
-  
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-if len(sys.argv) != 3: 
-    print "Correct usage: script, IP address, port number"
-    exit() 
-IP_address = str(sys.argv[1]) 
-Port = int(sys.argv[2]) 
-server.connect((IP_address, Port)) 
-  
-while True: 
-  
-    # maintains a list of possible input streams 
-    sockets_list = [sys.stdin, server] 
-  
+# Python program to implement client side of chat room.
+import socket
+import select
+import sys
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if len(sys.argv) != 3:
+    print("Correct usage: script, IP address, port number")
+    exit()
+IP_address = str(sys.argv[1])
+Port = int(sys.argv[2])
+server.connect((IP_address, Port))
+
+while True:
+
+    # maintains a list of possible input streams
+    sockets_list = [sys.stdin, server]
+
     """ There are two possible input situations. Either the 
     user wants to give  manual input to send to other people, 
     or the server is sending a message  to be printed on the 
@@ -25,16 +25,18 @@ while True:
     to send a message, then the if condition will hold true 
     below.If the user wants to send a message, the else 
     condition will evaluate as true"""
-    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[]) 
-  
-    for socks in read_sockets: 
-        if socks == server: 
-            message = socks.recv(2048) 
-            print message 
-        else: 
-            message = sys.stdin.readline() 
-            server.send(message) 
-            sys.stdout.write("<You>") 
-            sys.stdout.write(message) 
-            sys.stdout.flush() 
-server.close() 
+    read_sockets, write_socket, error_socket = select.select(
+        sockets_list, [], [])
+
+    for socks in read_sockets:
+        if socks == server:
+            message = socks.recv(2048)
+            message = str(message, 'utf-8')
+            print(message)
+        else:
+            message = sys.stdin.readline()
+            server.send(message.encode())
+            sys.stdout.write("<You>")
+            sys.stdout.write(message)
+            sys.stdout.flush()
+server.close()
