0f2c035ffa6d5a8851bdd5b739eef43102496f6c
Lanndon Rose <roselan@arch06.cis.gvsu.edu>
2018-11-09 13:47:42 -0500

    added encypt,decrpy but havent tested

--- lab2_client.c
+++ lab2_client.c
@@ -15,11 +15,13 @@
 #include <string.h>
 
 //lab2_client.c
+unsigned char sym_key[32];
 
 void handleErrors(void) {
 	ERR_print_errors_fp(stderr);
 	abort();
 }
+
 
 int rsa_encrypt(unsigned char* in, size_t inlen, EVP_PKEY *key, unsigned char* out) {
 	EVP_PKEY_CTX *ctx;
@@ -67,7 +69,7 @@ int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key, u
 	if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
 		handleErrors();
 	plaintext_len = len;
-	if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len))
+	if(1 != EVP_DecryptFinal_ex(ctxunsigned char 
 		handleErrors();
 	plaintext_len += len;
 	EVP_CIPHER_CTX_free(ctx);
@@ -76,10 +78,18 @@ int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key, u
 
 void* handlestuff(void* arg) {
 	int socket = *(int*)arg;
-	char line[5000];
-	int n = recv(socket, line, 5000, 0);
-	printf("\n%s\n", line);
-	if(line[0]=='Q'&&line[1]=='u'&&line[2]=='i'&&line[3]=='t') {
+	char line[5016];
+	char D_message[5000];
+	int n = recv(socket, line, 5016, 0);
+
+	unsigned char iv[16];
+	strncpy(iv,line,16);
+	decrypt(line[strlen(line)-5000],strlen(line),sym_key,iv,D_message);
+
+
+
+	printf("\n%s\n", D_message);
+	if(D_message[0]=='Q'&&D_message[1]=='u'&&D_message[2]=='i'&&D_message[3]=='t') {
 		shutdown(socket, SHUT_RDWR);
 		close(socket);
 		exit(0);
@@ -90,6 +100,8 @@ void* handlestuff(void* arg) {
 void* handlestuff2(void* arg) {
 	int socket = *(int*)arg;
 	char line[5000];
+	char E_message[5000];
+	char Final[5016];
 	//scanf("%s", line);
 	int m = read(STDIN_FILENO, line, 5000);
 	for(int a = 0; a < 5000; a++) {
@@ -97,9 +109,14 @@ void* handlestuff2(void* arg) {
 			line[a]=='\0';
 		}
 	}
+	unsigned char iv[16];
+	RAND_bytes(iv,16);
 
+	encrypt(line,strlen((char*)line),sym_key,iv,E_message);
 
-	send(socket, line, strlen(line)+1, 0);
+	strcat(Final,iv);
+	strcat(Final,E_message);
+	send(socket, Final, strlen(Final)+1, 0);
 	if(line[0]=='Q'&&line[1]=='u'&&line[2]=='i'&&line[3]=='t') {
 		exit(0);
 	}
@@ -138,39 +155,20 @@ int main(int argc, char** argv){
 		printf("There was an error connecting\n");
 		return 2;
 	}
-	//while(1) {
-		//find file to send
-		//printf("Enter a message: ");
-		//char message[5000]
-		//scanf("%s", message);
+
 	
-		//char* message = malloc(sizeof(char)*5000);
-		//FILE* fp;
+	unsigned char E_key[256];
 
-		//make sure file exists
-		//while((fp = fopen(filename, "r")) == (FILE*)NULL) {
-		//	printf("Filename does not exist, try again: \n");
-		//	scanf("%s", filename);
-		//}
-		
-		//sending file
-		//fgets(message, 5000, fp);
-		//send(sockfd, message, strlen(message)+1, 0);
-		//free(message);
-		//fclose(fp);
+	FILE* fp = fopen("RSApub.pem","rb");
+	EVP_PKEY *PublicKey;
+	PublicKey = PEM_read_PUBKEY(fp,NULL,NULL,NULL);
 
-		//recieving file
-		//char recmessage[5000];
-		//recv(sockfd, recmessage, 5000, 0);
-		//FILE* fp2;
+	RAND_bytes(sym_key,32);
 
-		//writing message to new file
-		//fp2 = fopen("recievedMessage", "w");
-		//fprintf(fp2, "%s", recmessage);
-		//fclose(fp2);
-		//printf("recieved from server: %s\n", recmessage);
-	//}
-	//close socket
+	rsa_encrypt(sym_key,32,PublicKey,E_key);
+
+	send(sockfd,E_key,strlen(E_key)+1,0);
+
 	while(1) {
 	
 		fflush(stdin);
--- tcpechoserver.c
+++ tcpechoserver.c
@@ -18,176 +18,243 @@
 
 int sockfd;
 int clist[10];
-unsigned char symkey[32];
+int kList[];
 
-void handleErrors(void) {
+EVP_PKEY *PrivateKey; 
+
+void handleErrors(void)
+{
 	ERR_print_errors_fp(stderr);
 	abort();
 }
 
-int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key, unsigned char *iv, unsigned char *ciphertext) {
+int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key, unsigned char *iv, unsigned char *ciphertext)
+{
 	EVP_CIPHER_CTX *ctx;
 	int len;
 	int ciphertext_len;
-	if(!(ctx = EVP_CIPHER_CTX_new()))
+	if (!(ctx = EVP_CIPHER_CTX_new()))
 		handleErrors();
-	if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+	if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
 		handleErrors();
-	if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+	if (1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
 		handleErrors();
 	ciphertext_len = len;
-	if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
+	if (1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
 		handleErrors();
 	ciphertext_len += len;
 	EVP_CIPHER_CTX_free(ctx);
 	return ciphertext_len;
 }
 
-int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char* key, unsigned char *iv, unsigned char *plaintext) {
+int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key, unsigned char *iv, unsigned char *plaintext)
+{
 	EVP_CIPHER_CTX *ctx;
 	int len;
 	int plaintext_len;
-	if(!(ctx = EVP_CIPHER_CTX_new()))
+	if (!(ctx = EVP_CIPHER_CTX_new()))
 		handleErrors();
-	if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+	if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
 		handleErrors();
-	if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
+	if (1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
 		handleErrors();
 	plaintext_len = len;
-	if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len))
+	if (1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len))
 		handleErrors();
 	EVP_CIPHER_CTX_free(ctx);
 	return plaintext_len;
 }
 
-int rsa_decrypt(unsigned char* in, size_t inlen, EVP_PKEY *key, unsigned char* out) {
+int rsa_decrypt(unsigned char *in, size_t inlen, EVP_PKEY *key, unsigned char *out)
+{
 	EVP_PKEY_CTX *ctx;
 	size_t outlen;
 	ctx = EVP_PKEY_CTX_new(key, NULL);
-	if(!ctx)
+	if (!ctx)
 		handleErrors();
-	if(EVP_PKEY_decrypt_init(ctx) <= 0)
+	if (EVP_PKEY_decrypt_init(ctx) <= 0)
 		handleErrors();
-	if(EVP_PKEY_CTX_set_rsa_padding(ctx, RSA_PKCS1_OAEP_PADDING) <= 0)
+	if (EVP_PKEY_CTX_set_rsa_padding(ctx, RSA_PKCS1_OAEP_PADDING) <= 0)
 		handleErrors();
-	if(EVP_PKEY_decrypt(ctx, NULL, &outlen, in, inlen) <= 0)
+	if (EVP_PKEY_decrypt(ctx, NULL, &outlen, in, inlen) <= 0)
 		handleErrors();
-	if(EVP_PKEY_decrypt(ctx, out, &outlen, in, inlen) <= 0)
+	if (EVP_PKEY_decrypt(ctx, out, &outlen, in, inlen) <= 0)
 		handleErrors();
 	return outlen;
 }
 
-void* handleclient(void* arg) {
-	int clientsocket = *(int*)arg;
-	char line[5000];
-	int n = recv(clientsocket, line, 5000, 0);
-	printf("\nRecieved from client: %s\n",line);
-	if(line[0]=='Q'&&line[1]=='u'&&line[2]=='i'&&line[3]=='t') {
+void *handleclient(void *arg)
+{
+	int clientsocket = *(int *)arg;
+	char line[5016];
+	char D_message[5000];
+	int n = recv(clientsocket, line, 5016, 0);
+	unsigned char tempKey[32];
+	for( int i = 0; i < 10; i++){
+		if(clist[i] == clientsocket){
+			snprinf(tempKey,sizeof(tempKey),kList[i]);
+		}
+	}
+	unsigned char iv[16];
+	strncpy(iv,line,16);
+	decrypt(line[strlen(line)-5000],strlen(line),tempKey,iv,D_message);
+
+	printf("\nRecieved from client: %s\n", D_message);
+	if (D_message[0] == 'Q' && D_message[1] == 'u' && D_message[2] == 'i' && D_message[3] == 't')
+	{
 		exit(0);
 	}
 	//close(clientsocket);
 }
 
-void* handleclient2(void* arg) {
-	int socket = *(int*)arg;
+void *handleclient2(void *arg)
+{
+	int socket = *(int *)arg;
 	char line[5000];
-//	scanf("%s", line);
+	char E_message[7000];
+	char Final[5016];
+	//	scanf("%s", line);
 	int m = read(STDIN_FILENO, line, 5000);
-	for(int a = 0; a < 5000; a++) {
-		if(line[a] == '\n') {
-			line[a]=='\0';
+	for (int a = 0; a < 5000; a++)
+	{
+		if (line[a] == '\n')
+		{
+			line[a] == '\0';
 		}
 	}
 
-	send(socket, line, strlen(line)+1,0);
-	if(line[0]=='Q'&&line[1]=='u'&&line[2]=='i'&&line[3]=='t') {
+	unsigned char tempKey[32];
+	for( int i = 0; i < 10; i++){
+		if(clist[i] == socket){
+			snprinf(tempKey,sizeof(tempKey),kList[i]);
+		}
+	}
+
+	unsigned char iv[16];
+	RAND_bytes(iv,16);
+
+	encrypt(line,strlen((char*)line),tempKey,iv,E_message);
+
+	strcat(Final,iv);
+	strcat(Final,E_message);
+
+	send(socket, line, strlen(line) + 1, 0);
+	if (line[0] == 'Q' && line[1] == 'u' && line[2] == 'i' && line[3] == 't')
+	{
 		exit(0);
 	}
-//	close(socket);
+	//	close(socket);
 }
 
-void* addclient(void* arg) {
-	int clientsocket = *(int*)arg;
+void *addclient(void *arg)
+{
+	int clientsocket = *(int *)arg;
+	unsigned char dKey[32];
+	unsigned char key[256];
+	recv(clientsocket, key, 256, 0);
+	int decryptLength = rsa_decrypt(key,32,PrivateKey,dKey);
 	//clist.append(clientsocket);
 	int size = 10;
-	for(int j = 0; j < size; j++) {
-		if((int)clist[j] < 0) {
-			clist[j]=clientsocket;
+	for (int j = 0; j < size; j++)
+	{
+		if ((int)clist[j] < 0)
+		{
+			clist[j] = clientsocket;
+			kList[j] = dKey;
+
 			break;
 		}
 	}
-	while(1) {
+	while (1)
+	{
 		char line[5000];
 		int n = recv(clientsocket, line, 5000, 0);
-		if(line[0] == '~') {
-			char* menu = "Console Opened\n(d#)DM another user, (l)List users, or (k)kill server";
-			int m = send(clientsocket, menu, strlen(menu)+1, 0);
+		if (line[0] == '~')
+		{
+			char *menu = "Console Opened\n(d#)DM another user, (l)List users, or (k)kill server";
+			int m = send(clientsocket, menu, strlen(menu) + 1, 0);
 			char ans[5];
 			recv(clientsocket, ans, 5, 0);
 			fflush(stdout);
 			fflush(stdin);
-			if(ans[0]=='k') {
+			if (ans[0] == 'k')
+			{
 				//kill server
 				char newline[5000];
 				snprintf(newline, sizeof(newline), "Quit\0");
-				for(int i = 0; i < 10; i++) {
-					if(clist[i]>0) {
-						send((int)clist[i], newline, strlen(newline)+1, 0);
+				for (int i = 0; i < 10; i++)
+				{
+					if (clist[i] > 0)
+					{
+						send((int)clist[i], newline, strlen(newline) + 1, 0);
 					}
 				}
 				//sleep(3);
 				exit(0);
 			}
-			if(ans[0]=='l') {
-				for(int x = 0; x < 10; x++) {
-					if(clist[x]>0) {
+			if (ans[0] == 'l')
+			{
+				for (int x = 0; x < 10; x++)
+				{
+					if (clist[x] > 0)
+					{
 						//printf("%d..%d", x, clist[x]);
 						char newline[5000];
 						snprintf(newline, sizeof(newline), "%d -- (ID)%d", x, clist[x]);
-						send(clientsocket, newline, strlen(newline)+1, 0);
+						send(clientsocket, newline, strlen(newline) + 1, 0);
 						sleep(1);
 					}
 				}
 			}
-			if(ans[0]=='d') {
+			if (ans[0] == 'd')
+			{
 				//dm someone
 				char getdm[5000];
-			       	snprintf(getdm, sizeof(getdm), "Enter message to send privately");
-				send(clientsocket, getdm, strlen(getdm)+1, 0);
+				snprintf(getdm, sizeof(getdm), "Enter message to send privately");
+				send(clientsocket, getdm, strlen(getdm) + 1, 0);
 				char dm[5000];
 				recv(clientsocket, dm, 5000, 0);
-				
-				for(int i = 0; i < 10; i++) {
-					if(clist[i] == (int)ans[1]-48) {
-						send((int)clist[i], dm, strlen(dm)+1, 0);
+
+				for (int i = 0; i < 10; i++)
+				{
+					if (clist[i] == (int)ans[1] - 48)
+					{
+						send((int)clist[i], dm, strlen(dm) + 1, 0);
 					}
 				}
 			}
-			if(ans[0] == 'r') {
+			if (ans[0] == 'r')
+			{
 				char rmsg[4];
 				snprintf(rmsg, sizeof(rmsg), "Quit");
 
-				for(int i = 0; i < 10; i++) {
-					if(clist[i]==(int)ans[1]-48) {
+				for (int i = 0; i < 10; i++)
+				{
+					if (clist[i] == (int)ans[1] - 48)
+					{
 						//char extra[5000];
 						//close(clist[i]);
-						send((int)clist[i], rmsg, strlen(rmsg)+1, 0);
+						send((int)clist[i], rmsg, strlen(rmsg) + 1, 0);
 						//recv(clist[i], extra, 5000, 0);
 						shutdown((int)clist[i], SHUT_RDWR);
 						close((int)clist[i]);
-						clist[i]=-20;
-						return;
+						clist[i] = -20;
+						kList[i] = 0;
+						return 0;
 					}
 				}
 			}
 		}
-		else {
-			printf("\n<%d> says: %s", clientsocket,line);
+		else
+		{
+			printf("\n<%d> says: %s", clientsocket, line);
 			char newline[5000];
 			snprintf(newline, sizeof(newline), "<%d> posts: %s", clientsocket, line);
-			for(int i = 0; i < size; i++) {
-				if(clist[i]>0) {
-					int m = send((int)clist[i], newline, strlen(newline)+1, 0);
+			for (int i = 0; i < size; i++)
+			{
+				if (clist[i] > 0)
+				{
+					int m = send((int)clist[i], newline, strlen(newline) + 1, 0);
 					//printf("sent to %d", clist[i]);
 					//printf("message sent to %d", clientsocket);
 				}
@@ -198,33 +265,39 @@ void* addclient(void* arg) {
 	close(clientsocket);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
+	
+	FILE* fp = fopen("RSApriv.pem","rb");
+	PrivateKey = PEM_read_PrivateKey(fp,NULL,NULL,NULL);
 
-	for(int x = 0; x < 10; x++) {
-		clist[x]=-20;
-	}
+	for (int x = 0; x < 10; x++)
+	{
+		clist[x] = -20;
+	};
 
 	//creating socket
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	struct sockaddr_in serveraddr, clientaddr;
-	serveraddr.sin_family=AF_INET;
-	
+	serveraddr.sin_family = AF_INET;
+
 	//reading in port number
 	int portnum;
 	printf("Enter port number: ");
 	scanf("%d", &portnum);
 	//printf("\nListening...\n");
 
-	serveraddr.sin_port=htons(portnum);
-	serveraddr.sin_addr.s_addr=INADDR_ANY;	
-	bind(sockfd, (struct sockaddr*)&serveraddr,sizeof(serveraddr));
-	listen(sockfd,10);
+	serveraddr.sin_port = htons(portnum);
+	serveraddr.sin_addr.s_addr = INADDR_ANY;
+	bind(sockfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+	listen(sockfd, 10);
 
 	int len = sizeof(clientaddr);
 	//int clientsocket = accept(sockfd, (struct sockaddr*)&clientaddr, &len);
-		
-	while(1) {
-		int clientsocket = accept(sockfd, (struct sockaddr*)&clientaddr, &len);
+
+	while (1)
+	{
+		int clientsocket = accept(sockfd, (struct sockaddr *)&clientaddr, &len);
 		//fflush(stdin);
 		//fflush(stdout);
 		//printf("\ntype away\n");
@@ -233,13 +306,13 @@ int main(int argc, char** argv) {
 		pthread_t child;
 		pthread_create(&child, NULL, addclient, &clientsocket);
 		pthread_detach(child);
-		
+
 		//printf("\ntype away\n");
 		//pthread_t child2;
 		//pthread_create(&child2, NULL, handleclient2, &clientsocket);
 		//pthread_detach(child2);
-	//}
-	//while(1) {
+		//}
+		//while(1) {
 	}
 
 	close(sockfd);
